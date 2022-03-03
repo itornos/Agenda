58695026d42b898bb1b0bb9837e284a0aacffac4
itornos <0315itornos@e-itaca.es>
2022-03-03 21:29:12 +0100

    cerrar fichero

--- persona/src/persona/Ficheros.java
+++ persona/src/persona/Ficheros.java
@@ -38,7 +38,6 @@ public class Ficheros {
 		Persona persona = new Persona();//objeto de clase jugador para acceder a los valores de la clase
 
 		int contador=0;//contador de palabras
-		boolean repetido=true;//bucle
 		FileReader fichero = null;//fichero a observar
 		int caracter;//valor leido
 		String palabra = "";//cadena de caracteres leidos
@@ -46,7 +45,6 @@ public class Ficheros {
 		//buscar fichero, en caso de error se mustra el mensaje y termina el metodo
 		try {
 			fichero = new FileReader("persona.txt");
-			repetido = false;
 		} catch (Exception e) {
 			System.out.println("no se ha encotrdao es fichero");
 			return;
@@ -61,23 +59,23 @@ public class Ficheros {
 				contador++;//Numero de palabras obtenidas
 			    switch(contador) {
 			    	case 1:
-			    		persona.DNINumero =Integer.parseInt(palabra);break;//1º Palabra
+			    		persona.DNINumero =Integer.parseInt(palabra);break;//1ï¿½ Palabra
 			    	case 2:
-			    		persona.DNILetra = palabra;break;//2º Palabra
+			    		persona.DNILetra = palabra;break;//2ï¿½ Palabra
 			    	case 3:
-						persona.nombre =palabra;break;//3º Palabra
+						persona.nombre =palabra;break;//3ï¿½ Palabra
 			    	case 4:
-						persona.apellido1=palabra;break;//4º Palabra
+						persona.apellido1=palabra;break;//4ï¿½ Palabra
 			    	case 5:
-						persona.apellido2=palabra;break;//5º Palabra
+						persona.apellido2=palabra;break;//5ï¿½ Palabra
 			    	case 6:
-						persona.sexo=palabra;break;//6º Palabra			
+						persona.sexo=palabra;break;//6ï¿½ Palabra			
 			    	case 7:
-						persona.edad=Integer.parseInt(palabra);break;//7º Palabra
+						persona.edad=Integer.parseInt(palabra);break;//7ï¿½ Palabra
 			    	case 8:
-						persona.cod_tlf=Integer.parseInt(palabra);break;//8º Palabra	
+						persona.cod_tlf=Integer.parseInt(palabra);break;//8ï¿½ Palabra	
 			    	case 9:
-						persona.tlf=Integer.parseInt(palabra);//9º Palabra	
+						persona.tlf=Integer.parseInt(palabra);//9ï¿½ Palabra	
 						contador=0;//reseteo para nueva importacion de la siguiente linea
 						
 						//limpiar salto de linea del fichero
@@ -89,7 +87,7 @@ public class Ficheros {
 							cont++;
 						}
 						
-						//Se pasan los valores obtenidos a un objeto persona y se añaden a la lista
+						//Se pasan los valores obtenidos a un objeto persona y se aï¿½aden a la lista
 						Persona rPersona = new Persona(persona.DNINumero,persona.DNILetra, persona.nombre, persona.apellido1, persona.apellido2, persona.sexo, persona.edad, persona.cod_tlf, persona.tlf);
 						Persona.lista.add(rPersona);
 						break;
@@ -97,5 +95,6 @@ public class Ficheros {
 				palabra = "";//reset de palabra para meter un campo nuevo
 			}
 		}
+		fichero.close();
 	}
 }
